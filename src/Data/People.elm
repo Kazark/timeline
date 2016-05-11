0ef7580230c8853ff5978b0acf611cefdf776cce
Keith Pinson <kazark@zoho.com>
2016-05-10 21:53:41 -0400

    Condense the manner in which the data is stored

--- src/Data/People.elm
+++ src/Data/People.elm
@@ -8,812 +8,276 @@ johnOwen =
   { from = duringYear 1616, to = on 1683 Aug 24, label = "John Owen" }
 
 
-goodwinBorn =
-  on 1600 Oct 5
-
-
-goodwinDied =
-  on 1679 Feb 23
-
-
 thomasGoodwin =
-  { from = goodwinBorn, to = goodwinDied, label = "Thomas Goodwin" }
-
-
-perkinsBorn =
-  duringYear 1558
-
-
-perkinsDied =
-  duringYear 1602
+  { from = on 1600 Oct 5, to = on 1679 Feb 23, label = "Thomas Goodwin" }
 
 
 williamPerkins =
-  { from = perkinsBorn, to = perkinsDied, label = "William Perkins" }
-
-
-amesBorn =
-  duringYear 1576
-
-
-amesDied =
-  on 1633 Nov 14
+  { from = duringYear 1558, to = duringYear 1602, label = "William Perkins" }
 
 
 williamAmes =
-  { from = amesBorn, to = amesDied, label = "William Ames" }
-
-
-flavelBorn =
-  duringYear 1627
-
-
-flavelDied =
-  duringYear 1691
+  { from = duringYear 1576, to = on 1633 Nov 14, label = "William Ames" }
 
 
 johnFlavel =
-  { from = flavelBorn, to = flavelDied, label = "John Flavel" }
-
-
-rutherfordBorn =
-  duringYear 1600
-
-
-rutherfordDied =
-  on 1661 Mar 29
+  { from = duringYear 1627, to = duringYear 1691, label = "John Flavel" }
 
 
 samuelRutherford =
-  { from = rutherfordBorn, to = rutherfordDied, label = "Samuel Rutherford" }
-
-
-bunyanBorn =
-  on 1628 Nov 30
-
-
-bunyanDied =
-  on 1688 Aug 31
+  { from = duringYear 1600, to = on 1661 Mar 29, label = "Samuel Rutherford" }
 
 
 johnBunyan =
-  { from = bunyanBorn, to = bunyanDied, label = "John Bunyan" }
-
-
-bostonBorn =
-  on 1676 Mar 17
-
-
-bostonDied =
-  on 1732 May 20
+  { from = on 1628 Nov 30, to = on 1688 Aug 31, label = "John Bunyan" }
 
 
 thomasBoston =
-  { from = bostonBorn, to = bostonDied, label = "Thomas Boston" }
-
-
-watsonBorn =
-  duringYear 1620
-
-
-watsonDied =
-  duringYear 1686
+  { from = on 1676 Mar 17, to = on 1732 May 20, label = "Thomas Boston" }
 
 
 thomasWatson =
-  { from = watsonBorn, to = watsonDied, label = "Thomas Watson" }
-
-
-loveBorn =
-  duringYear 1618
-
-
-loveDied =
-  on 1651 Aug 22
+  { from = duringYear 1620, to = duringYear 1686, label = "Thomas Watson" }
 
 
 christopherLove =
-  { from = loveBorn, to = loveDied, label = "Christopher Love" }
-
-
-turretinBorn =
-  on 1623 Oct 17
-
-
-turretinDied =
-  on 1687 Sep 28
+  { from = duringYear 1618, to = on 1651 Aug 22, label = "Christopher Love" }
 
 
 francisTurretin =
-  { from = turretinBorn, to = turretinDied, label = "Francis Turretin" }
-
-
-witsiusBorn =
-  on 1636 Feb 12
-
-
-witsiusDied =
-  on 1708 Oct 22
+  { from = on 1623 Oct 17, to = on 1687 Sep 28, label = "Francis Turretin" }
 
 
 hermannWitsius =
-  { from = witsiusBorn, to = witsiusDied, label = "Hermann Witsius" }
-
-
-brainerdBorn =
-  on 1718 Apr 20
-
-
-brainerdDied =
-  on 1747 Oct 9
+  { from = on 1636 Feb 12, to = on 1708 Oct 22, label = "Hermann Witsius" }
 
 
 davidBrainerd =
-  { from = brainerdBorn, to = brainerdDied, label = "David Brainerd" }
-
-
-swinnockBorn =
-  duringYear 1627
-
-
-swinnockDied =
-  duringYear 1673
+  { from = on 1718 Apr 20, to = on 1747 Oct 9, label = "David Brainerd" }
 
 
 georgeSwinnock =
-  { from = swinnockBorn, to = swinnockDied, label = "George Swinnock" }
-
-
-sibbesBorn =
-  duringYear 1577
-
-
-sibbesDied =
-  duringYear 1635
+  { from = duringYear 1627, to = duringYear 1673, label = "George Swinnock" }
 
 
 richardSibbes =
-  { from = sibbesBorn, to = sibbesDied, label = "Richard Sibbes" }
-
-
-cottonBorn =
-  on 1585 Dec 4
-
-
-cottonDied =
-  on 1652 Dec 23
+  { from = duringYear 1577, to = duringYear 1635, label = "Richard Sibbes" }
 
 
 johnCotton =
-  { from = cottonBorn, to = cottonDied, label = "John Cotton" }
-
-
-rmatherBorn =
-  duringYear 1596
-
-
-rmatherDied =
-  on 1669 Apr 22
+  { from = on 1585 Dec 4, to = on 1652 Dec 23, label = "John Cotton" }
 
 
 richardMather =
-  { from = rmatherBorn, to = rmatherDied, label = "Richard Mather" }
-
-
-imatherBorn =
-  on 1639 Jun 21
-
-
-imatherDied =
-  on 1723 Aug 23
+  { from = duringYear 1596, to = on 1669 Apr 22, label = "Richard Mather" }
 
 
 increaseMather =
-  { from = imatherBorn, to = imatherDied, label = "Increase Mather" }
-
-
-cmatherBorn =
-  on 1663 Feb 12
-
-
-cmatherDied =
-  on 1728 Feb 13
+  { from = on 1639 Jun 21, to = on 1723 Aug 23, label = "Increase Mather" }
 
 
 cottonMather =
-  { from = cmatherBorn, to = cmatherDied, label = "Cotton Mather" }
-
-
-charnockBorn =
-  duringYear 1628
-
-
-charnockDied =
-  duringYear 1680
+  { from = on 1663 Feb 12, to = on 1728 Feb 13, label = "Cotton Mather" }
 
 
 stephenCharnock =
-  { from = charnockBorn, to = charnockDied, label = "Stephen Charnock" }
-
-
-knoxBorn =
-  duringYear 1513
-
-
-knoxDied =
-  on 1572 Nov 24
+  { from = duringYear 1628, to = duringYear 1680, label = "Stephen Charnock" }
 
 
 johnKnox =
-  { from = knoxBorn, to = knoxDied, label = "John Knox" }
-
-
-renwickBorn =
-  on 1662 Feb 15
-
-
-renwickDied =
-  on 1688 Feb 17
+  { from = duringYear 1513, to = on 1572 Nov 24, label = "John Knox" }
 
 
 jamesRenwick =
-  { from = renwickBorn, to = renwickDied, label = "James Renwick" }
-
-
-tyndaleBorn =
-  duringYear 1494
-
-
-tyndaleDied =
-  duringYear 1536
+  { from = on 1662 Feb 15, to = on 1688 Feb 17, label = "James Renwick" }
 
 
 williamTyndale =
-  { from = tyndaleBorn, to = tyndaleDied, label = "William Tyndale" }
-
-
-wycliffeBorn =
-  duringYear 1320
-
-
-wycliffeDied =
-  on 1384 Dec 31
+  { from = duringYear 1494, to = duringYear 1536, label = "William Tyndale" }
 
 
 johnWycliffe =
-  { from = wycliffeBorn, to = wycliffeDied, label = "John Wycliffe" }
-
-
-husBorn =
-  duringYear 1369
-
-
-husDied =
-  on 1415 Jul 6
+  { from = duringYear 1320, to = on 1384 Dec 31, label = "John Wycliffe" }
 
 
 janHus =
-  { from = husBorn, to = husDied, label = "Jan Hus" }
-
-
-lutherBorn =
-  on 1483 Nov 10
-
-
-lutherDied =
-  on 1546 Feb 18
+  { from = duringYear 1369, to = on 1415 Jul 6, label = "Jan Hus" }
 
 
 martinLuther =
-  { from = lutherBorn, to = lutherDied, label = "Martin Luther" }
-
-
-zwingliBorn =
-  on 1484 Jan 1
-
-
-zwingliDied =
-  on 1531 Oct 11
+  { from = on 1483 Nov 10, to = on 1546 Feb 18, label = "Martin Luther" }
 
 
 huldrychZwingli =
-  { from = zwingliBorn, to = zwingliDied, label = "Huldrych Zwingli" }
-
-
-calvinBorn =
-  on 1509 Jul 10
-
-
-calvinDied =
-  on 1564 May 27
+  { from = on 1484 Jan 1, to = on 1531 Oct 11, label = "Huldrych Zwingli" }
 
 
 johnCalvin =
-  { from = calvinBorn, to = calvinDied, label = "John Calvin" }
-
-
-bezaBorn =
-  on 1519 Jun 24
-
-
-bezaDied =
-  on 1605 Oct 13
+  { from = on 1509 Jul 10, to = on 1564 May 27, label = "John Calvin" }
 
 
 theodoreBeza =
-  { from = bezaBorn, to = bezaDied, label = "Theodore Beza" }
-
-
-melanchthonBorn =
-  on 1497 Feb 16
-
-
-melanchthonDied =
-  on 1560 Apr 19
+  { from = on 1519 Jun 24, to = on 1605 Oct 13, label = "Theodore Beza" }
 
 
 philipMelanchthon =
-  { from = melanchthonBorn, to = melanchthonDied, label = "Philip Melanchthon" }
-
-
-ursinusBorn =
-  on 1534 Jul 18
-
-
-ursinusDied =
-  on 1583 May 6
+  { from = on 1497 Feb 16, to = on 1560 Apr 19, label = "Philip Melanchthon" }
 
 
 zachariasUrsinus =
-  { from = ursinusBorn, to = ursinusDied, label = "Zacharias Ursinus" }
-
-
-abrakelBorn =
-  on 1635 Jan 2
-
-
-abrakelDied =
-  on 1711 Oct 30
+  { from = on 1534 Jul 18, to = on 1583 May 6, label = "Zacharias Ursinus" }
 
 
 wilhelmusABrakel =
-  { from = abrakelBorn, to = abrakelDied, label = "Wilhelmus a Brakel" }
-
-
-waldoBorn =
-  duringYear 1140
-
-
-waldoDied =
-  duringYear 1205
+  { from = on 1635 Jan 2, to = on 1711 Oct 30, label = "Wilhelmus a Brakel" }
 
 
 peterWaldo =
-  { from = waldoBorn, to = waldoDied, label = "Peter Waldo" }
-
-
-edwardsBorn =
-  on 1703 Oct 5
-
-
-edwardsDied =
-  on 1758 Mar 22
+  { from = duringYear 1140, to = duringYear 1205, label = "Peter Waldo" }
 
 
 jonathanEdwards =
-  { from = edwardsBorn, to = edwardsDied, label = "Jonathan Edwards" }
-
-
-ballBorn =
-  duringMonth 1585 Oct
-
-
-ballDied =
-  on 1640 Oct 20
+  { from = on 1703 Oct 5, to = on 1758 Mar 22, label = "Jonathan Edwards" }
 
 
 johnBall =
-  { from = ballBorn, to = ballDied, label = "John Ball" }
-
-
-ryleBorn =
-  on 1816 May 10
-
-
-ryleDied =
-  on 1900 Jun 10
+  { from = duringMonth 1585 Oct, to = on 1640 Oct 20, label = "John Ball" }
 
 
 jcRyle =
-  { from = ryleBorn, to = ryleDied, label = "JC Ryle" }
-
-
-chodgeBorn =
-  on 1797 Dec 27
-
-
-chodgeDied =
-  on 1878 Jun 19
+  { from = on 1816 May 10, to = on 1900 Jun 10, label = "JC Ryle" }
 
 
 charlesHodge =
-  { from = chodgeBorn, to = chodgeDied, label = "Charles Hodge" }
-
-
-bavinckBorn =
-  on 1854 Dec 13
-
-
-bavinckDied =
-  on 1921 Jul 29
+  { from = on 1797 Dec 27, to = on 1878 Jun 19, label = "Charles Hodge" }
 
 
 hermanBavinck =
-  { from = bavinckBorn, to = bavinckDied, label = "Herman Bavinck" }
-
-
-gvosBorn =
-  on 1862 Mar 14
-
-
-gvosDied =
-  on 1949 Aug 13
+  { from = on 1854 Dec 13, to = on 1921 Jul 29, label = "Herman Bavinck" }
 
 
 geerhardusVos =
-  { from = gvosBorn, to = gvosDied, label = "Geerhardus Vos" }
-
-
-vantilBorn =
-  on 1895 May 3
-
-
-vantilDied =
-  on 1987 Apr 17
+  { from = on 1862 Mar 14, to = on 1949 Aug 13, label = "Geerhardus Vos" }
 
 
 corneliusVanTil =
-  { from = vantilBorn, to = vantilDied, label = "Cornelius Van Til" }
-
-
-murrayBorn =
-  on 1898 Oct 14
-
-
-murrayDied =
-  on 1975 May 8
+  { from = on 1895 May 3, to = on 1987 Apr 17, label = "Cornelius Van Til" }
 
 
 johnMurray =
-  { from = murrayBorn, to = murrayDied, label = "John Murray" }
-
-
-machenBorn =
-  on 1881 Jul 28
-
-
-machenDied =
-  on 1937 Jan 1
+  { from = on 1898 Oct 14, to = on 1975 May 8, label = "John Murray" }
 
 
 jGreshamMachen =
-  { from = machenBorn, to = machenDied, label = "J Gresham Machen" }
-
-
-burroughsBorn =
-  duringYear 1600
-
-
-burroughsDied =
-  on 1646 Nov 13
+  { from = on 1881 Jul 28, to = on 1937 Jan 1, label = "J Gresham Machen" }
 
 
 jeremiahBurroughs =
-  { from = burroughsBorn, to = burroughsDied, label = "Jeremiah Burroughs" }
-
-
-anselmBorn =
-  duringYear 1033
-
-
-anselmDied =
-  on 1109 Apr 21
+  { from = duringYear 1600, to = on 1646 Nov 13, label = "Jeremiah Burroughs" }
 
 
 anselmOfCanterbury =
-  { from = anselmBorn, to = anselmDied, label = "Anselm of Canterbury" }
+  { from = duringYear 1033, to = on 1109 Apr 21, label = "Anselm of Canterbury" }
 
 
 thomasAquinas =
   { from = duringYear 1225, to = on 1274 Mar 7, label = "Thomas Aquinas" }
 
 
-augustineBorn =
-  on 354 Nov 13
-
-
-augustineDied =
-  on 430 Aug 28
-
-
 augustineOfHippo =
-  { from = augustineBorn, to = augustineDied, label = "Augustine of Hippo" }
-
-
-boethiusBorn =
-  duringYear 480
-
-
-boethiusDied =
-  duringYear 524
+  { from = on 354 Nov 13, to = on 430 Aug 28, label = "Augustine of Hippo" }
 
 
 aniciusBoethius =
-  { from = boethiusBorn, to = boethiusDied, label = "Anicius Boethius" }
-
-
-athanasiusBorn =
-  duringYear 297
-
-
-athanasiusDied =
-  on 373 May 2
+  { from = duringYear 480, to = duringYear 524, label = "Anicius Boethius" }
 
 
 athanasiusOfAlexandria =
-  { from = athanasiusBorn, to = athanasiusDied, label = "Athanasius of Alexandria" }
-
-
-chrysostomBorn =
-  duringYear 349
-
-
-chrysostomDied =
-  duringYear 407
+  { from = duringYear 297, to = on 373 May 2, label = "Athanasius of Alexandria" }
 
 
 johnChrysostom =
-  { from = chrysostomBorn, to = chrysostomDied, label = "John Chrysostom" }
-
-
-arminiusBorn =
-  on 1560 Oct 10
-
-
-arminiusDied =
-  on 1609 Oct 19
+  { from = duringYear 349, to = duringYear 407, label = "John Chrysostom" }
 
 
 jacobusArminius =
-  { from = arminiusBorn, to = arminiusDied, label = "Jacobus Arminius" }
-
-
-nazianzusBorn =
-  duringYear 329
-
-
-nazianzusDied =
-  on 390 Jan 25
+  { from = on 1560 Oct 10, to = on 1609 Oct 19, label = "Jacobus Arminius" }
 
 
 gregoryNazianzus =
-  { from = nazianzusBorn, to = nazianzusDied, label = "Gregory of Nazianzus" }
-
-
-martyrBorn =
-  duringYear 100
-
-
-martyrDied =
-  duringYear 165
+  { from = duringYear 329, to = on 390 Jan 25, label = "Gregory of Nazianzus" }
 
 
 justinMartyr =
-  { from = martyrBorn, to = martyrDied, label = "Justin Martyr" }
-
-
-brownHBorn =
-  duringYear 1722
-
-
-brownHDied =
-  on 1787 Jun 19
+  { from = duringYear 100, to = duringYear 165, label = "Justin Martyr" }
 
 
 johnBrownOfHaddington =
-  { from = brownHBorn, to = brownHDied, label = "John Brown of Haddington" }
-
-
-spurgeonBorn =
-  on 1834 Jun 19
-
-
-spurgeonDied =
-  on 1892 Jan 31
+  { from = duringYear 1722, to = on 1787 Jun 19, label = "John Brown of Haddington" }
 
 
 charlesSpurgeon =
-  { from = spurgeonBorn, to = spurgeonDied, label = "Charles Spurgeon" }
-
-
-kuyperBorn =
-  on 1837 Oct 29
-
-
-kuyperDied =
-  on 1920 Nov 8
+  { from = on 1834 Jun 19, to = on 1892 Jan 31, label = "Charles Spurgeon" }
 
 
 abrahamKuyper =
-  { from = kuyperBorn, to = kuyperDied, label = "Abraham Kuyper" }
-
-
-hegelBorn =
-  on 1770 Aug 27
-
-
-hegelDied =
-  on 1831 Nov 14
+  { from = on 1837 Oct 29, to = on 1920 Nov 8, label = "Abraham Kuyper" }
 
 
 gwfHegel =
-  { from = hegelBorn, to = hegelDied, label = "GWF Hegel" }
-
-
-kantBorn =
-  on 1724 Apr 22
-
-
-kantDied =
-  on 1804 Feb 12
+  { from = on 1770 Aug 27, to = on 1831 Nov 14, label = "GWF Hegel" }
 
 
 immanuelKant =
-  { from = kantBorn, to = kantDied, label = "Immanuel Kant" }
-
-
-barthBorn =
-  on 1886 May 10
-
-
-barthDied =
-  on 1968 Dec 10
+  { from = on 1724 Apr 22, to = on 1804 Feb 12, label = "Immanuel Kant" }
 
 
 karlBarth =
-  { from = barthBorn, to = barthDied, label = "Karl Barth" }
-
-
-kierkegaardBorn =
-  on 1813 May 5
-
-
-kierkegaardDied =
-  on 1855 Nov 11
+  { from = on 1886 May 10, to = on 1968 Dec 10, label = "Karl Barth" }
 
 
 sorenKierkegaard =
-  { from = kierkegaardBorn, to = kierkegaardDied, label = "Soren Kierkegaard" }
-
-
-reidBorn =
-  on 1710 Apr 26
-
-
-reidDied =
-  on 1796 Oct 7
+  { from = on 1813 May 5, to = on 1855 Nov 11, label = "Soren Kierkegaard" }
 
 
 thomasReid =
-  { from = reidBorn, to = reidDied, label = "Thomas Reid" }
-
-
-harnackBorn =
-  on 1851 May 7
-
-
-harnackDied =
-  on 1930 Jun 10
+  { from = on 1710 Apr 26, to = on 1796 Oct 7, label = "Thomas Reid" }
 
 
 adolfVonHarnack =
-  { from = harnackBorn, to = harnackDied, label = "Adolf von Harnack" }
-
-
-schleiermacherBorn =
-  on 1768 Nov 21
-
-
-schleiermacherDied =
-  on 1834 Feb 12
+  { from = on 1851 May 7, to = on 1930 Jun 10, label = "Adolf von Harnack" }
 
 
 friedrichSchleiermacher =
-  { from = schleiermacherBorn, to = schleiermacherDied, label = "Friedrich Schleiermacher" }
-
-
-klineBorn =
-  on 1922 Dec 15
-
-
-klineDied =
-  on 2007 Apr 14
+  { from = on 1768 Nov 21, to = on 1834 Feb 12, label = "Friedrich Schleiermacher" }
 
 
 meredithKline =
-  { from = klineBorn, to = klineDied, label = "Meredith Kline" }
-
-
-schaefferBorn =
-  on 1912 Jan 30
-
-
-schaefferDied =
-  on 1984 May 15
+  { from = on 1922 Dec 15, to = on 2007 Apr 14, label = "Meredith Kline" }
 
 
 francisSchaeffer =
-  { from = schaefferBorn, to = schaefferDied, label = "Francis Schaeffer" }
-
-
-bucerBorn =
-  on 1491 Nov 11
-
-
-bucerDied =
-  on 1551 Feb 28
+  { from = on 1912 Jan 30, to = on 1984 May 15, label = "Francis Schaeffer" }
 
 
 martinBucer =
-  { from = bucerBorn, to = bucerDied, label = "Martin Bucer" }
-
-
-lewisBorn =
-  on 1898 Nov 29
-
-
-lewisDied =
-  on 1963 Nov 22
+  { from = on 1491 Nov 11, to = on 1551 Feb 28, label = "Martin Bucer" }
 
 
 csLewis =
-  { from = lewisBorn, to = lewisDied, label = "CS Lewis" }
-
-
-lombardBorn =
-  duringYear 1096
-
-
-lombardDied =
-  duringMonth 1160 Jul
+  { from = on 1898 Nov 29, to = on 1963 Nov 22, label = "CS Lewis" }
 
 
 peterLombard =
-  { from = lombardBorn, to = lombardDied, label = "Peter Lombard" }
-
-
-abelardBorn =
-  duringYear 1079
-
-
-abelardDied =
-  on 1142 Apr 21
+  { from = duringYear 1096, to = duringMonth 1160 Jul, label = "Peter Lombard" }
 
 
 peterAbelard =
-  { from = abelardBorn, to = abelardDied, label = "Peter Abelard" }
-
-
-haldaneBorn =
-  on 1764 Feb 28
-
-
-haldaneDied =
-  on 1842 Dec 12
+  { from = duringYear 1079, to = on 1142 Apr 21, label = "Peter Abelard" }
 
 
 robertHaldane =
-  { from = haldaneBorn, to = haldaneDied, label = "Robert Haldane" }
+  { from = on 1764 Feb 28, to = on 1842 Dec 12, label = "Robert Haldane" }
 
 
 williamOfOckham =
