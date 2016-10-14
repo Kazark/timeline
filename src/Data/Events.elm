326723fbb9935d595fcf65d3e5fc3562b371973b
Kazark <kazark@zoho.com>
2016-10-14 08:11:31 -0400

    Catch this up with my learnings

--- src/Data/Events.elm
+++ src/Data/Events.elm
@@ -10,5 +10,9 @@ events =
   , event (occurred <| duringMonth 1572 Aug        ) "St. Bartholomew's Day massacre"
   , event (occurred <| duringYear  1563            ) "Heidelberg Catechism"
   , event (occurred <| on          1662 May 19     ) "Act of Uniformity"
+  , event (occurred <| duringYear  1662            ) "Half-Way Covenant"
   , event (occurred <| on          1689 May 24     ) "Act of Toleration"
+  , event (occurred <| duringYear  1834            ) "Auchterarder case"
+  , event (occurred <| on          1843 May 18     ) "Disruption of 1843"
+  , event (occurred <| duringYear  1887            ) "Downgrade controversy"
   ]
